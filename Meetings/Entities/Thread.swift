fbac94e8f2ea5698bf0160e5c63133c46a4e9010
Yu357 <forestgreen14689@gmail.com>
2022-07-21 21:44:43 +0900

    feat: Thread構造体を作成

---
+++ Meetings/Entities/Thread.swift
@@ -0,0 +1,15 @@
+//
+//  Thread.swift
+//  Meetings
+//
+//  Created by Yu on 2022/07/21.
+//
+
+import Foundation
+
+struct Thread: Identifiable {
+    let id: String
+    let title: String
+    let createdAt: Date
+    let userId: String
+}
