11799b07ccb997b993fa078c6e11ddfb2530c96e
Yu357 <forestgreen14689@gmail.com>
2022-07-21 21:59:55 +0900

    feat: Threadドキュメントを読みとった

--- Meetings/Views/FirstView.swift
+++ Meetings/Views/FirstView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct FirstView: View {
     
+    @ObservedObject private var threadsViewModel = ThreadsViewModel()
+    
     @State private var isShowProfileView = false
     @State private var isShowCreateThreadView = false
     
@@ -16,8 +18,11 @@ struct FirstView: View {
         NavigationView {
             
             List {
-                NavigationLink(destination: ThreadView()) {
-                    Text("hello")
+//                NavigationLink(destination: ThreadView()) {
+//                    Text("hello")
+//                }
+                ForEach(threadsViewModel.threads) { thread in
+                    Text(thread.title)
                 }
             }
             
