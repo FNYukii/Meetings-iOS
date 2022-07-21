ab811a8228d2e3b9559f5ec5dcfefba141209f69
Yu357 <forestgreen14689@gmail.com>
2022-07-21 20:36:17 +0900

    Initial Commit

---
+++ Meetings/MeetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingsApp.swift
+//  Meetings
+//
+//  Created by Yu on 2022/07/21.
+//
+
+import SwiftUI
+
+@main
+struct MeetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
