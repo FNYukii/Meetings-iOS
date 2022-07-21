7e598021a8581e98013f0b9907a5b8b78c084c80
Yu357 <forestgreen14689@gmail.com>
2022-07-21 21:04:03 +0900

    feat: CreateCommentViewへの遷移を設定

--- Meetings/Views/ThreadView.swift
+++ Meetings/Views/ThreadView.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct ThreadView: View {
+    
+    @State private var isShowCreateCommentView = false
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+            .sheet(isPresented: $isShowCreateCommentView) {
+                CreateCommentView()
+            }
             
             .navigationTitle("hello")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        isShowCreateCommentView.toggle()
+                    }) {
+                        Image(systemName: "square.and.pencil")
+                    }
+                }
+            }
         
     }
 }
