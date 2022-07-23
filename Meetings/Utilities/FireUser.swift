d3b42fafec8c99bc29b4c2905cb311ceb0119094
Yu357 <forestgreen14689@gmail.com>
2022-07-23 15:07:30 +0900

    feat: FireUserクラスを作成

---
+++ Meetings/Utilities/FireUser.swift
@@ -0,0 +1,29 @@
+//
+//  FireUser.swift
+//  Meetings
+//
+//  Created by Yu on 2022/07/23.
+//
+
+import Firebase
+
+class FireUser {
+    
+    static func createUser(userId: String, displayName: String, userTag: String, iconPath: String?) {
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(userId)
+            .setData([
+                "displayName": displayName,
+                "userTag": userTag,
+                "iconPath": iconPath as Any,
+            ]) { err in
+                if let err = err {
+                    print("HELLO! Fail! Error writing User: \(err)")
+                } else {
+                    print("HELLO! Success! Created 1 User.")
+                }
+            }
+    }
+    
+}
