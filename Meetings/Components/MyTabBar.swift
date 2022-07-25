8e7e89f0cfa9f63e7c4f2c97b5d08d8a7671eca7
Yu357 <forestgreen14689@gmail.com>
2022-07-25 22:49:03 +0900

    feat: MyTabBarをスタイリング

--- Meetings/Components/MyTabBar.swift
+++ Meetings/Components/MyTabBar.swift
@@ -54,6 +54,7 @@ struct MyTabBar: View {
                     .buttonStyle(.plain)
                 }
             }
+            .padding(.top)
             
             // Underline Row
             Divider()
