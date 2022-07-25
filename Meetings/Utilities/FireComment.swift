2fa58f8d320c043d6c0f9a9342e620922f4ece5d
Yu357 <forestgreen14689@gmail.com>
2022-07-25 23:23:38 +0900

    feat: readLikedCommentsメソッドを用意

--- Meetings/Utilities/FireComment.swift
+++ Meetings/Utilities/FireComment.swift
@@ -68,12 +68,42 @@ class FireComment {
                 
                 // Return
                 completion?(comments)
-        }
+            }
     }
     
     static func readLikedComments(userId: String, completion: (([Comment]) -> Void)?) {
-        // ドキュメント読み取り
-        
+        // Userドキュメントを読み取り
+        FireUser.readUser(userId: userId) { user in
+            // userがnilなら終了
+            if user == nil {
+                return
+            }
+            
+            // likedCommentIdsの値を取得
+            let likedCommentIds = user!.likedCommentIds
+            
+            // ユーザーがいいねしたコメントを全て取得
+            let db = Firestore.firestore()
+            db.collection("comments")
+                .whereField(FieldPath.documentID(), in: likedCommentIds)
+                .getDocuments() { (querySnapshot, err) in
+                    if let err = err {
+                        print("HELLO! Fail! Error Reeding Comments: \(err)")
+                        return
+                    }
+                    print("HELLO! Success! Read \(querySnapshot!.count) Comments.")
+                    
+                    // Comments
+                    var comments: [Comment] = []
+                    for document in querySnapshot!.documents {
+                        let comment = toComment(document: document)
+                        comments.append(comment)
+                    }
+                    
+                    // Return
+                    completion?(comments)
+                }
+        }
     }
     
     static func createComment(threadId: String, text: String) {
