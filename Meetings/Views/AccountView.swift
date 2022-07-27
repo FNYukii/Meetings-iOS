579861729e999b0738cfaa004c89527d507a7f33
Yu357 <forestgreen14689@gmail.com>
2022-07-27 16:28:49 +0900

    feat: EditProfileViewへの遷移を設定

--- Meetings/Views/AccountView.swift
+++ Meetings/Views/AccountView.swift
@@ -16,43 +16,56 @@ struct AccountView: View {
     @State private var isShowDialog = false
     
     var body: some View {
-        
-        List {
-            Section {
-                HStack {
-                    Text("email")
-                        .foregroundColor(.secondary)
-                    Spacer()
-                    Text(FireAuth.userEmail() != nil ? FireAuth.userEmail()! : "")
+        NavigationView {
+            
+            List {
+                Section {
+                    HStack {
+                        Text("email")
+                            .foregroundColor(.secondary)
+                        Spacer()
+                        Text(FireAuth.userEmail() != nil ? FireAuth.userEmail()! : "")
+                    }
+                    
+                    HStack {
+                        Text("password")
+                            .foregroundColor(.secondary)
+                        Spacer()
+                        Text("••••")
+                    }
                 }
                 
-                HStack {
-                    Text("password")
-                        .foregroundColor(.secondary)
-                    Spacer()
-                    Text("••••")
+                Section {
+                    Button("sign_out") {
+                        isShowDialog.toggle()
+                    }
+                    .foregroundColor(.red)
+                    .frame(maxWidth: .infinity, alignment: .center)
                 }
             }
             
-            Section {
-                Button("sign_out") {
-                    isShowDialog.toggle()
+            .confirmationDialog("", isPresented: $isShowDialog, titleVisibility: .hidden) {
+                Button("sign_out", role: .destructive) {
+                    FireAuth.signOut()
+                    dismiss()
                 }
-                .foregroundColor(.red)
-                .frame(maxWidth: .infinity, alignment: .center)
+            } message: {
+                Text("are_you_sure_you_want_to_sign_out")
+            }
+            
+            .navigationTitle("account")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        Text("done")
+                            .fontWeight(.bold)
+                    }
+                }
             }
         }
-        
-        .confirmationDialog("", isPresented: $isShowDialog, titleVisibility: .hidden) {
-            Button("sign_out", role: .destructive) {
-                FireAuth.signOut()
-                dismiss()
-            }
-        } message: {
-            Text("are_you_sure_you_want_to_sign_out")
-        }
-        
-        .navigationTitle("account")
-        .navigationBarTitleDisplayMode(.inline)
+        .navigationViewStyle(.stack)
     }
 }
