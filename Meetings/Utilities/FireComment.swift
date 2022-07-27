ce98dd22f165ffd45d6ce59f96f990d157459581
Yu357 <forestgreen14689@gmail.com>
2022-07-27 10:22:45 +0900

    clean: Todoコメントを追記

--- Meetings/Utilities/FireComment.swift
+++ Meetings/Utilities/FireComment.swift
@@ -215,6 +215,9 @@ class FireComment {
                 completion?([])
             }
             
+            // TODO: 配列commentsをいいねした順番に並べ替えてからReturnする
+            // TODO: キャッシュから取得した配列commentsを瞬時にReturnし、その後サーバーから取得した配列commentsをReturnする
+            
             // likedCommentIdsの数だけ、ドキュメント読み取りを行う
             var likedComments: [Comment] = []
             likedCommentIds.forEach { commentId in
