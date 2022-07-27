845af4d34e7d33b830ef69e0b5c535ab9737bb11
Yu357 <forestgreen14689@gmail.com>
2022-07-27 16:44:06 +0900

    feat

--- Meetings/Views/EditProfileView.swift
+++ Meetings/Views/EditProfileView.swift
@@ -11,12 +11,14 @@ struct EditProfileView: View {
     
     // Environments
     @Environment(\.dismiss) private var dismiss
-    
+        
     // States
     @State private var displayName = ""
     @State private var userTag = ""
     @State private var introduction = ""
-    @State private var icon: UIImage? = nil
+    @State private var iconUrl: String? = nil
+    
+    @State private var isLoadedUser = false
     
     var body: some View {
         NavigationView {
@@ -43,7 +45,18 @@ struct EditProfileView: View {
                 
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button(action: {
-                        dismiss()
+                        // Userドキュメントを更新
+                        FireUser.updateUser(displayName: displayName, userTag: userTag, introduction: introduction, iconUrl: nil) { documentId in
+                            // 失敗
+                            if documentId == nil {
+                                
+                            }
+                            
+                            // 成功
+                            if documentId != nil {
+                                dismiss()
+                            }
+                        }
                     }) {
                         Text("done")
                             .fontWeight(.bold)
@@ -52,5 +65,22 @@ struct EditProfileView: View {
             }
         }
         .navigationViewStyle(.stack)
+        .onAppear(perform: load)
+    }
+    
+    private func load() {
+        if !isLoadedUser {
+            FireUser.readUser(userId: FireAuth.uid()!) { user in
+                // 失敗
+                
+                // 成功
+                if let user = user {
+                    self.displayName = user.displayName
+                    self.userTag = user.userTag
+                    self.introduction = user.introduction
+                    self.iconUrl = user.iconUrl
+                }
+            }
+        }
     }
 }
