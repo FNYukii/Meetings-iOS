b8f40d0df056cd46e14536e4999b1988d185afb8
Yu357 <forestgreen14689@gmail.com>
2022-07-28 23:56:26 +0900

    feat: 画像をグリッド表示してみた

--- Meetings/Components/CommentRow.swift
+++ Meetings/Components/CommentRow.swift
@@ -113,8 +113,42 @@ struct CommentRow: View {
                     .fixedSize(horizontal: false, vertical: true)
                 
                 // Images Row
-                ForEach(comment.imageUrls, id: \.self) { imageUrl in
-                    CommentImage(url: imageUrl)
+                Group {
+                    if comment.imageUrls.count == 1 {
+                        CommentImage(url: comment.imageUrls.first)
+                    }
+                    
+                    if comment.imageUrls.count == 2 {
+                        HStack {
+                            CommentImage(url: comment.imageUrls.first)
+                            CommentImage(url: comment.imageUrls[1])
+                        }
+                    }
+                    
+                    if comment.imageUrls.count == 3 {
+                        VStack {
+                            HStack {
+                                CommentImage(url: comment.imageUrls.first)
+                                CommentImage(url: comment.imageUrls[1])
+                            }
+                            
+                            CommentImage(url: comment.imageUrls[2])
+                        }
+                    }
+                    
+                    if comment.imageUrls.count == 4 {
+                        VStack {
+                            HStack {
+                                CommentImage(url: comment.imageUrls.first)
+                                CommentImage(url: comment.imageUrls[1])
+                            }
+                            
+                            HStack {
+                                CommentImage(url: comment.imageUrls[2])
+                                CommentImage(url: comment.imageUrls[3])
+                            }
+                        }
+                    }
                 }
                 
                 // Reaction Row
