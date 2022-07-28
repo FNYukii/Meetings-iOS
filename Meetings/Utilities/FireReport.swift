d415128441921281c33f1a9771f916d5702c6a1c
Yu357 <forestgreen14689@gmail.com>
2022-07-28 13:36:16 +0900

    feat: FireReportファイルを作成

---
+++ Meetings/Utilities/FireReport.swift
@@ -0,0 +1,14 @@
+//
+//  FireReport.swift
+//  Meetings
+//
+//  Created by Yu on 2022/07/28.
+//
+
+class FireReport {
+    
+    static func createReport(target: String, category: String, detail: String, completion: ((String?) -> Void)?) {
+        
+    }
+    
+}
