ddc5d9bd46b8e41f16a03e92833de2f75aa373bc
Yu357 <forestgreen14689@gmail.com>
2022-07-29 14:45:11 +0900

    feat: 画像の歪みを修正

--- Meetings/Views/ImageView.swift
+++ Meetings/Views/ImageView.swift
@@ -10,17 +10,35 @@ import SDWebImageSwiftUI
 
 struct ImageView: View {
     
+    // Environments
+    @Environment(\.dismiss) private var dismiss
+    
     // Image to show
     let url: String?
     
     var body: some View {
         
-        WebImage(url: URL(string: url ?? ""))
-            .resizable()
-            .placeholder {
-                Color.secondary
-                    .opacity(0.2)
-            }
-        
+        NavigationView {
+            WebImage(url: URL(string: url ?? ""))
+                .resizable()
+                .placeholder {
+                    Color.secondary
+                        .opacity(0.2)
+                }
+                .scaledToFit()
+                
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {
+                            dismiss()
+                        }) {
+                            Text("done")
+                                .fontWeight(.bold)
+                        }
+                    }
+                }
+        }
+        .navigationViewStyle(.stack)
     }
 }
