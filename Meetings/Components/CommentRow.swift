8a1560f4db34e3ec1a2db675f01a5c2d9ce27f27
Yu357 <forestgreen14689@gmail.com>
2022-07-29 10:46:18 +0900

    clean: CommentUserNameAndTagStackをコンポーネント化

--- Meetings/Components/CommentRow.swift
+++ Meetings/Components/CommentRow.swift
@@ -18,11 +18,7 @@ struct CommentRow: View {
     let isAbleShowingCommentView: Bool
     
     @State private var isShowProfileView = false
-    
-    // States
-    @State private var user: User? = nil
-    @State private var isLoadedUser = false
-        
+            
     var body: some View {
         HStack(alignment: .top, spacing: 8) {
             
@@ -37,43 +33,18 @@ struct CommentRow: View {
             
             // Content Column
             VStack(alignment: .leading) {
-                
                 // Header Row
                 HStack {
-                    // Progress views
-                    if !isLoadedUser {
-                        Color.secondary
-                            .opacity(0.2)
-                            .frame(width: 80)
-                        
-                        Color.secondary
-                            .opacity(0.2)
-                            .frame(width: 80)
-                    }
+                    // Display name & User tag Column
+                    CommentUserNameAndTagStack(comment: comment, stackFamily: .hstack)
                     
-                    // User reading failed views
-                    if isLoadedUser && user == nil {
-                        Image(systemName: "exclamationmark.triangle")
-                            .foregroundColor(.secondary)
-                        Text("user_reading_failed")
-                            .foregroundColor(.secondary)
-                    }
-                    
-                    // Display name & User tag
-                    if isLoadedUser && user != nil {
-                        Text(user!.displayName)
-                            .fontWeight(.bold)
-                        
-                        Text("@\(user!.userTag)")
-                            .foregroundColor(.secondary)
-                    }
-                    
-                    // Date text
+                    // Date Column
                     EditDate.howManyAgoText(from: comment.createdAt)
                         .foregroundColor(.secondary)
                     
                     Spacer()
                     
+                    // Menu Column
                     CommentMenu(comment: comment)
                 }
                 
@@ -104,16 +75,5 @@ struct CommentRow: View {
             }
         )
         .background(NavigationLink("", destination: CommentView(comment: comment)).disabled(!isAbleShowingCommentView).opacity(0))
-        .onAppear(perform: load)
-    }
-    
-    private func load() {
-        // Commentを追加したUserを読み取り
-        if user == nil {
-            FireUser.readUser(userId: comment.userId) { user in
-                self.user = user
-                self.isLoadedUser = true
-            }
-        }
     }
 }
