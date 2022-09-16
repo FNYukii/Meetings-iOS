b005a414868a530c046f5a82df19b2a7b5b415ca
Yu357 <forestgreen14689@gmail.com>
2022-09-16 21:46:13 +0900

    feat: TextFieldをScrollViewに内包

--- Meetings/Views/CreateThreadView.swift
+++ Meetings/Views/CreateThreadView.swift
@@ -25,13 +25,15 @@ struct CreateThreadView: View {
             
             List {
                 // TextField Row
-                TextField("title", text: $title)
-                    .introspectTextField { textField in
-                        textField.becomeFirstResponder()
-                    }
-                    .disabled(isLoading)
-                    .submitLabel(.done)
-                    .listRowSeparator(.hidden)
+                ScrollView(.horizontal) {
+                    TextField("title", text: $title)
+                        .introspectTextField { textField in
+                            textField.becomeFirstResponder()
+                        }
+                        .disabled(isLoading)
+                        .submitLabel(.done)
+                        .listRowSeparator(.hidden)
+                }
                 
                 // Tags Row
                 ForEach(0 ..< tags.count, id: \.self) { index in
