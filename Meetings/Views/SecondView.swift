d6feabbd2331232aae5181da0ed38dd38437c50f
Yu357 <forestgreen14689@gmail.com>
2022-09-20 17:06:46 +0900

    feat: おすすめタグを表示した

--- Meetings/Views/SecondView.swift
+++ Meetings/Views/SecondView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct SecondView: View {
     
+    // Search Bar
     @ObservedObject var searchBar: SearchBar = SearchBar()
+    
+    // Values
+    let recommendedTags = ["雑談", "天神祭", "ゲーム", "休日", "初心者"]
     
     var body: some View {
         NavigationView {
             List {
-                Text("Hello")
+                Section (header: Text("おすすめ")) {
+                    ForEach(recommendedTags, id: \.self) { tag in
+                        TagRow(name: tag)
+                            .listRowSeparator(.hidden, edges: .top)
+                            .listRowSeparator(.visible, edges: .bottom)
+                    }
+                }
             }
+            .listStyle(.plain)
             
             .add(searchBar)
             .onChange(of: searchBar.text) { _ in
