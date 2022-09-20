bd50562aef09274ab11e160840b0785a00e61b71
Yu357 <forestgreen14689@gmail.com>
2022-09-20 22:49:45 +0900

    feat: ResultViewへ遷移できた

---
+++ Meetings/Views/ResultView.swift
@@ -0,0 +1,17 @@
+//
+//  ResultView.swift
+//  Meetings
+//
+//  Created by Yu on 2022/09/20.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+            .navigationTitle("Result")
+            .navigationBarTitleDisplayMode(.inline)
+    }
+}
--- Meetings/Views/SecondView.swift
+++ Meetings/Views/SecondView.swift
@@ -9,11 +9,9 @@ import SwiftUI
 
 struct SecondView: View {
     
-    // Search Bar
-    @State private var keyword = ""
-    
     // States
-    @ObservedObject private var threadsByKeywordViewModel = ThreadsByKeywordViewModel()
+    @State private var keyword = ""
+    @State private var isShowResultView = false
     
     // Values
     private let recommendedTags = ["雑談", "天神祭", "ゲーム", "休日", "初心者"]
@@ -21,41 +19,41 @@ struct SecondView: View {
     var body: some View {
         NavigationView {
             List {
-                // Searched Threads Section
-                Section {
-                    // Progress
-                    if !keyword.isEmpty && !threadsByKeywordViewModel.isLoaded {
-                        ProgressView()
-                            .progressViewStyle(.circular)
-                            .frame(maxWidth: .infinity, alignment: .center)
-                            .listRowSeparator(.hidden)
-                    }
-                    
-                    // Failed
-                    if threadsByKeywordViewModel.isLoaded && threadsByKeywordViewModel.threads == nil {
-                        Text("threads_reading_failed")
-                            .frame(maxWidth: .infinity, alignment: .center)
-                            .foregroundColor(.secondary)
-                            .listRowSeparator(.hidden)
-                    }
-                    
-                    // No content
-                    if threadsByKeywordViewModel.isLoaded && threadsByKeywordViewModel.threads != nil && threadsByKeywordViewModel.threads!.count == 0 {
-                        Text("no_threads")
-                            .frame(maxWidth: .infinity, alignment: .center)
-                            .foregroundColor(.secondary)
-                            .listRowSeparator(.hidden)
-                    }
-                    
-                    // Done
-                    if threadsByKeywordViewModel.isLoaded && threadsByKeywordViewModel.threads != nil {
-                        ForEach(threadsByKeywordViewModel.threads!) { thread in
-                            ThreadRow(thread: thread)
-                                .listRowSeparator(.hidden, edges: .top)
-                                .listRowSeparator(.visible, edges: .bottom)
-                        }
-                    }
-                }
+//                // Searched Threads Section
+//                Section {
+//                    // Progress
+//                    if !keyword.isEmpty && !threadsByKeywordViewModel.isLoaded {
+//                        ProgressView()
+//                            .progressViewStyle(.circular)
+//                            .frame(maxWidth: .infinity, alignment: .center)
+//                            .listRowSeparator(.hidden)
+//                    }
+//
+//                    // Failed
+//                    if threadsByKeywordViewModel.isLoaded && threadsByKeywordViewModel.threads == nil {
+//                        Text("threads_reading_failed")
+//                            .frame(maxWidth: .infinity, alignment: .center)
+//                            .foregroundColor(.secondary)
+//                            .listRowSeparator(.hidden)
+//                    }
+//
+//                    // No content
+//                    if threadsByKeywordViewModel.isLoaded && threadsByKeywordViewModel.threads != nil && threadsByKeywordViewModel.threads!.count == 0 {
+//                        Text("no_threads")
+//                            .frame(maxWidth: .infinity, alignment: .center)
+//                            .foregroundColor(.secondary)
+//                            .listRowSeparator(.hidden)
+//                    }
+//
+//                    // Done
+//                    if threadsByKeywordViewModel.isLoaded && threadsByKeywordViewModel.threads != nil {
+//                        ForEach(threadsByKeywordViewModel.threads!) { thread in
+//                            ThreadRow(thread: thread)
+//                                .listRowSeparator(.hidden, edges: .top)
+//                                .listRowSeparator(.visible, edges: .bottom)
+//                        }
+//                    }
+//                }
                 
                 // Recommended Tags Section
                 if keyword.isEmpty {
@@ -70,9 +68,16 @@ struct SecondView: View {
             }
             .listStyle(.plain)
             
-            .searchable(text: $keyword)
-            .onChange(of: keyword) { _ in
-                threadsByKeywordViewModel.read(keyword: keyword)
+            .background(
+                NavigationLink(destination: ResultView(), isActive: $isShowResultView) {
+                    EmptyView()
+                }
+                .hidden()
+            )
+            
+            .searchable(text: $keyword, placement: .navigationBarDrawer(displayMode: .always), prompt: Text("keyword"))
+            .onSubmit(of: .search) {
+                isShowResultView.toggle()
             }
             
             .navigationTitle("search")
