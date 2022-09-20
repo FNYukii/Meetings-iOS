38c8713e345341e3d04021a814f9928a39383a46
Yu357 <forestgreen14689@gmail.com>
2022-09-20 17:23:02 +0900

    feat: 検索結果を表示できた

--- Meetings/Views/SecondView.swift
+++ Meetings/Views/SecondView.swift
@@ -10,14 +10,27 @@ import SwiftUI
 struct SecondView: View {
     
     // Search Bar
-    @ObservedObject var searchBar: SearchBar = SearchBar()
+    @ObservedObject private var searchBar: SearchBar = SearchBar()
+    
+    // States
+    @ObservedObject private var threadsByKeywordViewModel = ThreadsByKeywordViewModel()
     
     // Values
-    let recommendedTags = ["雑談", "天神祭", "ゲーム", "休日", "初心者"]
+    private let recommendedTags = ["雑談", "天神祭", "ゲーム", "休日", "初心者"]
     
     var body: some View {
         NavigationView {
             List {
+                // Searched Threads Section
+                Section {
+                    if threadsByKeywordViewModel.isLoaded && threadsByKeywordViewModel.threads != nil {
+                        ForEach(threadsByKeywordViewModel.threads!) { thread in
+                            ThreadRow(thread: thread)
+                        }
+                    }
+                }
+                
+                // Recommended Tags Section
                 Section (header: Text("おすすめ")) {
                     ForEach(recommendedTags, id: \.self) { tag in
                         TagRow(name: tag)
@@ -30,7 +43,7 @@ struct SecondView: View {
             
             .add(searchBar)
             .onChange(of: searchBar.text) { _ in
-                
+                threadsByKeywordViewModel.read(keyword: searchBar.text)
             }
             
             .navigationTitle("search")
