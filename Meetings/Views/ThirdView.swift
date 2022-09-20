d9785d0b1ea9b5231cc7888c896d4b6f9e74bf8b
Yu357 <forestgreen14689@gmail.com>
2022-09-21 02:38:14 +0900

    feat: 通知画面にメッセージを表示

--- Meetings/Views/ThirdView.swift
+++ Meetings/Views/ThirdView.swift
@@ -11,8 +11,12 @@ struct ThirdView: View {
     var body: some View {
         NavigationView {
             List {
-                
+                Text("no_notifications")
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .foregroundColor(.secondary)
+                    .listRowSeparator(.hidden)
             }
+            .listStyle(.plain)
             
             .navigationTitle("notifications")
             .navigationBarTitleDisplayMode(.inline)
