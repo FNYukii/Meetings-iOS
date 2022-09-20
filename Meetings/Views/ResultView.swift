21b18e9e07c10e5d5cbcd80a64028d730e7e7418
Yu357 <forestgreen14689@gmail.com>
2022-09-20 23:00:53 +0900

    feat: TabBarを配置

--- Meetings/Views/ResultView.swift
+++ Meetings/Views/ResultView.swift
@@ -11,6 +11,7 @@ struct ResultView: View {
     
     // States
     @State private var keyword = ""
+    @State private var selection = 0
     
     @ObservedObject private var threadsViewModel = ThreadsByKeywordViewModel()
     
@@ -21,36 +22,44 @@ struct ResultView: View {
     
     var body: some View {
         List {
-            // Progress
-            if !keyword.isEmpty && !threadsViewModel.isLoaded {
-                ProgressView()
-                    .progressViewStyle(.circular)
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .listRowSeparator(.hidden)
-            }
+            // Tab Bar Row
+            MyTabBar(tabBarItems: [Text("threads"), Text("comments"), Text("users")], selection: $selection)
+                .listRowSeparator(.hidden)
+                .listRowInsets(EdgeInsets())
+            
+            // Threads Row
+            Group {
+                // Progress
+                if !keyword.isEmpty && !threadsViewModel.isLoaded {
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .listRowSeparator(.hidden)
+                }
 
-            // Failed
-            if threadsViewModel.isLoaded && threadsViewModel.threads == nil {
-                Text("threads_reading_failed")
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .foregroundColor(.secondary)
-                    .listRowSeparator(.hidden)
-            }
+                // Failed
+                if threadsViewModel.isLoaded && threadsViewModel.threads == nil {
+                    Text("threads_reading_failed")
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .foregroundColor(.secondary)
+                        .listRowSeparator(.hidden)
+                }
 
-            // No content
-            if threadsViewModel.isLoaded && threadsViewModel.threads != nil && threadsViewModel.threads!.count == 0 {
-                Text("no_threads")
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .foregroundColor(.secondary)
-                    .listRowSeparator(.hidden)
-            }
+                // No content
+                if threadsViewModel.isLoaded && threadsViewModel.threads != nil && threadsViewModel.threads!.count == 0 {
+                    Text("no_threads")
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .foregroundColor(.secondary)
+                        .listRowSeparator(.hidden)
+                }
 
-            // Done
-            if threadsViewModel.isLoaded && threadsViewModel.threads != nil {
-                ForEach(threadsViewModel.threads!) { thread in
-                    ThreadRow(thread: thread)
-                        .listRowSeparator(.hidden, edges: .top)
-                        .listRowSeparator(.visible, edges: .bottom)
+                // Done
+                if threadsViewModel.isLoaded && threadsViewModel.threads != nil {
+                    ForEach(threadsViewModel.threads!) { thread in
+                        ThreadRow(thread: thread)
+                            .listRowSeparator(.hidden, edges: .top)
+                            .listRowSeparator(.visible, edges: .bottom)
+                    }
                 }
             }
         }
