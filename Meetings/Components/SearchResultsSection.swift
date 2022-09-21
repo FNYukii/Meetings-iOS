bc198f90010243e3bb4291db927c9fbbd00fff8b
Yu357 <forestgreen14689@gmail.com>
2022-09-21 10:15:12 +0900

    feat: SearchResultsSectionのFluentTabBarのタグ項目を削除

--- Meetings/Components/SearchResultsSection.swift
+++ Meetings/Components/SearchResultsSection.swift
@@ -16,7 +16,7 @@ struct SearchResultsSection: View {
     var body: some View {
         Section {
             // Tab Bar Row
-            FluentTabBar(tabBarItems: [Text("threads"), Text("tag"), Text("comments"), Text("users")], selection: $selection)
+            FluentTabBar(tabBarItems: [Text("threads"), Text("comments"), Text("users")], selection: $selection)
                 .listRowSeparator(.hidden)
                 .listRowInsets(EdgeInsets())
             
