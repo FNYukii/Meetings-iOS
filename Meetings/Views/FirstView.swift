add316381249b1c03e3ef29702fdb602a9ed127b
Yu357 <forestgreen14689@gmail.com>
2022-09-21 18:05:16 +0900

    clean: コードを読みやすく

--- Meetings/Views/FirstView.swift
+++ Meetings/Views/FirstView.swift
@@ -20,37 +20,40 @@ struct FirstView: View {
         NavigationView {
             
             List {
-                // Progress view
-                if !threadsViewModel.isLoaded {
-                    ProgressView()
-                        .progressViewStyle(.circular)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .listRowSeparator(.hidden)
-                }
-                
-                // Failed text
-                if threadsViewModel.isLoaded && threadsViewModel.threads == nil {
-                    Text("threads_reading_failed")
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .foregroundColor(.secondary)
-                        .listRowSeparator(.hidden)
-                }
-                
-                // No content text
-                if threadsViewModel.isLoaded && threadsViewModel.threads != nil && threadsViewModel.threads!.count == 0 {
-                    Text("no_threads")
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .foregroundColor(.secondary)
-                        .listRowSeparator(.hidden)
-                }
-                
-                // ThreadRows
-                if threadsViewModel.threads != nil {
-                    ForEach(threadsViewModel.threads!) { thread in
-                        ThreadRow(thread: thread)
+                // Recent Threads
+                Group {
+                    // Progress
+                    if !threadsViewModel.isLoaded {
+                        ProgressView()
+                            .progressViewStyle(.circular)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .listRowSeparator(.hidden)
                     }
-                    .listRowSeparator(.hidden, edges: .top)
-                    .listRowSeparator(.visible, edges: .bottom)
+                    
+                    // Failed
+                    if threadsViewModel.isLoaded && threadsViewModel.threads == nil {
+                        Text("threads_reading_failed")
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .foregroundColor(.secondary)
+                            .listRowSeparator(.hidden)
+                    }
+                    
+                    // No Results
+                    if threadsViewModel.isLoaded && threadsViewModel.threads != nil && threadsViewModel.threads!.count == 0 {
+                        Text("no_threads")
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .foregroundColor(.secondary)
+                            .listRowSeparator(.hidden)
+                    }
+                    
+                    // Done
+                    if threadsViewModel.threads != nil {
+                        ForEach(threadsViewModel.threads!) { thread in
+                            ThreadRow(thread: thread)
+                        }
+                        .listRowSeparator(.hidden, edges: .top)
+                        .listRowSeparator(.visible, edges: .bottom)
+                    }
                 }
             }
             .listStyle(.plain)
