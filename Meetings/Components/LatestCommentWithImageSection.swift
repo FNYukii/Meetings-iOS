69e974b09d960048356e1a48b3b7c5172ab01ea6
Yu357 <forestgreen14689@gmail.com>
2022-09-23 15:51:15 +0900

    feat: CommentViewへの遷移を設定

--- Meetings/Components/LatestCommentWithImageSection.swift
+++ Meetings/Components/LatestCommentWithImageSection.swift
@@ -10,13 +10,17 @@ import SDWebImageSwiftUI
 
 struct LatestCommentWithImageSection: View {
     
+    // States
     @State private var comment: Comment? = nil
     @State private var isLoaded = false
+    
+    // Navigations
+    @State private var isShowCommentView = false
     
     var body: some View {
         
         Button(action: {
-            
+            isShowCommentView.toggle()
         }) {
             // Progress
             if !isLoaded {
@@ -50,6 +54,12 @@ struct LatestCommentWithImageSection: View {
                     // Text Layer
                     Text(comment!.text)
                         .padding()
+                }
+                .background {
+                    NavigationLink(destination: CommentView(comment: comment!), isActive: $isShowCommentView) {
+                        EmptyView()
+                    }
+                    .hidden()
                 }
             }
         }
