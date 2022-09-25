97a4a235f28da405cd6f10114c092581e34e22d6
Yu357 <forestgreen14689@gmail.com>
2022-09-25 22:37:16 +0900

    feat: FireNotificationクラスを作成

---
+++ Meetings/Utilities/FireNotification.swift
@@ -0,0 +1,17 @@
+//
+//  FireNotification.swift
+//  Meetings
+//
+//  Created by Yu on 2022/09/25.
+//
+
+class FireNotification {
+    
+    static func createNotification() {
+        // TODO: notificationsコレクションに新しいドキュメントを追加
+    }
+    
+    static func deleteNotification(notificationId: String) {
+        // TODO: notificationsコレクションからドキュメントを削除
+    }
+}
